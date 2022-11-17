a7d927ca76ec6302c6fcd140710ddb109742d1b1
jisoo-o <cosmopark624@gmail.com>
2022-11-17 22:41:50 +0900

    [feature/service](#3) Add Storable
    src/main/java/com/LifeGame/service/Storable.java

---
+++ src/main/java/com/LifeGame/service/Storable.java
@@ -0,0 +1,12 @@
+package com.LifeGame.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public interface Storable {
+    void load(InputStream in) throws IOException;
+
+    void flush(OutputStream out) throws IOException;
+}
+
