216062004e58a9eb0da0f93843c902613d1ce50a
박서현 <maxmictic@gmail.com>
2022-11-18 16:03:10 +0900

    [feature/model](#2) test

--- src/main/java/com/LifeGame/model/ExampleModel.java
+++ src/main/java/com/LifeGame/model/ExampleModel.java
@@ -1,23 +1,23 @@
-package com.LifeGame.model;
-
-import org.springframework.stereotype.Component;
-
-import java.util.Observable;
-
-@Component
-public class ExampleModel extends Observable implements Model{
-    @Override
-    public void nextState() {
-
-    }
-
-    @Override
-    public void setMapSize(int n) {
-
-    }
-
-    @Override
-    public void toggle(int x, int y) {
-
-    }
-}
+//package com.LifeGame.model;
+//
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Observable;
+//
+//@Component
+//public class ExampleModel extends Observable implements Model{
+//    @Override
+//    public void nextState() {
+//
+//    }
+//
+//    @Override
+//    public void setMapSize(int n) {
+//
+//    }
+//
+//    @Override
+//    public void toggle(int x, int y) {
+//
+//    }
+//}
--- src/main/java/com/LifeGame/model/Model.java
+++ src/main/java/com/LifeGame/model/Model.java
@@ -1,7 +1,15 @@
 package com.LifeGame.model;
 
-public interface Model {
-    public void nextState();
-    public void setMapSize(int n);
-    public void toggle(int x, int y);   // dot가 업데이트된 위치
+//public interface Model {
+//    public void nextState();
+//    public void setMapSize(int n);
+//    public void toggle(int x, int y);   // dot가 업데이트된 위치
+//}
+
+public class Model {
+    public void nextState(){
+
+    }
+    public void setMapSize(int n){};
+    public void toggle(int x, int y){};   // dot가 업데이트된 위치
 }
