896d26783acf6b79c825c6e57e1ef9d676016c84
박서현 <maxmictic@gmail.com>
2022-11-20 20:32:49 +0900

    [feature/model](#2) add test examples src/test/java/com.LifeGame/model/ChangeStateTest.java

--- src/main/java/com/LifeGame/model/ChangeState.java
+++ src/main/java/com/LifeGame/model/ChangeState.java
@@ -1,12 +1,11 @@
 package com.LifeGame.model;
 
-
-// changestate : method
-// class : model -> interface의 것 옮겨오기 Or implements
 public class ChangeState implements Model{
 
     @Override
-    public void nextState(int[][] arr){}
+    public void nextState(int[][] arr){
+
+    }
 
     @Override
     public void setMapSize(int n) {
@@ -17,7 +16,7 @@ public class ChangeState implements Model{
     public void toggle(int x, int y) {
 
     }
-    public static void update(int[][] map) {
+    public static int[][] update(int[][] map) {
         int[] dx = {-1, -1, -1, 0, 1, 1, 1, 0};
         int[] dy = {-1, 0, 1, 1, 1, 0, -1, -1};
         int n = map.length;
@@ -46,51 +45,36 @@ public class ChangeState implements Model{
             }
         }
 
-//        for (int i = 0; i < map.length; i++) {
-//            int[] check = map[i];
+        return map;
+
+    }
+
+//    public static void main(String[] args) {
+//
+//        int[][] arr = {{1,0,1,0,1,0,1,1,0,0},{1,1,0,0,0,1,0,0,0,1},{1,1,1,0,0,0,0,0,0,0},{0,0,1,1,0,1,1,0,0,0},{0,0,0,1,0,0,1,0,0,0},
+//                {0,0,1,1,0,1,1,0,0,0},{0,0,0,1,0,0,1,0,0,0}, {1,0,1,0,1,0,1,1,0,0},{1,1,0,0,0,1,0,0,0,1},{1,1,1,0,0,0,0,0,0,0}};
+//
+//        System.out.println("before");
+//        for (int i = 0; i < arr.length; i++) {
+//            int[] check = arr[i];
 //            for (int j = 0; j < check.length; j++) {
 //                System.out.print(check[j] + " ");
 //            }
 //            System.out.println();
 //        }
-
-    }
-
-    public static void main(String[] args) {
-
-//        int[][] arr = new int[3][3];
 //
+//        long start = System.nanoTime();
+//        arr = update(arr);
+//        long end = System.nanoTime();
+//        System.out.println("수행시간: " + (end - start) + " ns");
 //
-//        // 1일 때 기준으로
-//        // 큰 사이즈에 대해서
-//        arr[0][0] = 1;
-//        arr[0][1] = 1;
-//        arr[0][2] = 1;
-//        arr[1][0] = 1;
-//        arr[1][1] = 0;
-//        arr[1][2] = 0;
-//        arr[2][0] = 0;
-//        arr[2][1] = 0;
-//        arr[2][2] = 1;
-
-
-        int[][] arr = {{1,0,1,0,1,0,1,1,0,0},{1,1,0,0,0,1,0,0,0,1},{1,1,1,0,0,0,0,0,0,0},{0,0,1,1,0,1,1,0,0,0},{0,0,0,1,0,0,1,0,0,0},
-                {0,0,1,1,0,1,1,0,0,0},{0,0,0,1,0,0,1,0,0,0}, {1,0,1,0,1,0,1,1,0,0},{1,1,0,0,0,1,0,0,0,1},{1,1,1,0,0,0,0,0,0,0}};
-
-        System.out.println("before");
-        for (int i = 0; i < arr.length; i++) {
-            int[] check = arr[i];
-            for (int j = 0; j < check.length; j++) {
-                System.out.print(check[j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("after");
-
-        long start = System.nanoTime();
-        update(arr);
-        long end = System.nanoTime();
-        System.out.println("수행시간: " + (end - start) + " ns");
-
-    }
+//        System.out.println("final");
+//        for (int i = 0; i < arr.length; i++) {
+//            int[] check = arr[i];
+//            for (int j = 0; j < check.length; j++) {
+//                System.out.print(check[j] + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
 }
--- src/test/java/com/LifeGame/model/ChangeStateTest.java
+++ src/test/java/com/LifeGame/model/ChangeStateTest.java
@@ -1,6 +1,7 @@
 package com.LifeGame.model;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,6 +14,25 @@ class ChangeStateTest {
     }
 
     @Test
+    @DisplayName("update")
     void update() {
+        //given
+        int[][] arr = {{1, 1, 1}, {1, 0, 0}, {0, 0, 1}};
+        int[][] arr2 = {{0, 0, 0, 0}, {1, 0, 1, 0}, {0, 1, 1, 0}, {0, 1, 0, 0}};
+        int[][] arr3 = {{0, 0, 0, 0, 0}, {0, 1, 1, 1, 0}, {0, 1, 0, 1, 0}, {0, 1, 1, 1, 0}, {0, 0, 0, 0, 0}};
+
+        int[][] ans = {{1, 1, 0}, {1, 0, 1}, {0, 0, 0}};
+        int[][] ans2 = {{0, 0, 0, 0}, {0, 0, 1, 0}, {1, 0, 1, 0}, {0, 1, 1, 0}};
+        int[][] ans3 = {{0, 0, 1, 0, 0}, {0, 1, 0, 1, 0}, {1, 0, 0, 0, 1}, {0, 1, 0, 1, 0}, {0, 0, 1, 0, 0}};
+
+        //when
+        arr = ChangeState.update(arr);
+        arr2 = ChangeState.update(arr2);
+        arr3 = ChangeState.update(arr3);
+
+        //then
+        assertArrayEquals(ans, arr);
+        assertArrayEquals(ans2, arr2);
+        assertArrayEquals(ans3, arr3);
     }
 }
