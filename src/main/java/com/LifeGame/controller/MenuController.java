f5a4ffb8ba50d1f535c36264d77d2304c0215fef
AllyHyeseongKim <ally3k@icloud.com>
2022-11-23 18:06:33 +0900

    [feature/controller](#1) Create MenuController
    src/main/java/com/LifeGame/controller/MenuController.java

--- src/main/java/com/LifeGame/controller/MenuController.java
+++ src/main/java/com/LifeGame/controller/MenuController.java
@@ -1,28 +1,39 @@
 package com.LifeGame.controller;
 
-import com.LifeGame.model.Model;
+import com.LifeGame.controller.action.Action;
+import com.LifeGame.view.LifePanel;
+import com.LifeGame.view.View;
+import com.LifeGame.view.menu.MenuBar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
+import javax.swing.*;
+import java.awt.*;
 
 @Component
 public class MenuController {
 
-    private final Model model;
-
-    private final HashMap<String, Runnable> actions = new HashMap<>();
+    private final View view;
+    private final MenuBar menuBar;
+    private final LifePanel lifePanel;
+    private boolean isViewInitialized;
 
     @Autowired
-    public MenuController(Model model) {
-        this.model = model;
+    public MenuController(View view, MenuBar menuBar, LifePanel lifePanel) {
+        this.view = view;
+        this.menuBar = menuBar;
+        this.lifePanel = lifePanel;
+        this.isViewInitialized = false;
     }
 
-    public void addAction(String name, Runnable action) {
-        this.actions.put(name, action);
-    }
+    public void addMenuItem(String menu, String menuItem, Action action) {
+        this.menuBar.addMenu(menu, menuItem, action);
 
-    public void actionHandler(String name) {
-        this.actions.get(name);
+        if (!this.isViewInitialized) {
+            this.isViewInitialized = true;
+            this.view.setJMenuBar((JMenuBar) this.menuBar);
+            this.view.getContentPane().add(this.lifePanel, BorderLayout.CENTER); //{=life.java.install}
+            this.view.pack();
+        }
     }
 }
